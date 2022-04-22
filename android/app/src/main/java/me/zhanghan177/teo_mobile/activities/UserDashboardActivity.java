b3233efd6ca4f2023b7261e8e92a4bc2eef36b90
Han Zhang <han@flightorflight.com>
2022-04-22 11:04:20 -0400

    Untrack libteo

---
+++ android/app/src/main/java/me/zhanghan177/teo_mobile/activities/UserDashboardActivity.java
@@ -0,0 +1,14 @@
+package me.zhanghan177.teo_mobile.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard);
+    }
+}
--- libteo/docs/mainpage.md
+++
@@ -1,9 +0,0 @@
-# Documentation for Modern Library                             {#mainpage}
-
-This is the documentation for my simple example library.
-
-It is good documentation because:
-
-1. It exists.
-2. I wrote it.
-
